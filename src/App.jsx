a5063c554030dfabf8091d0f48b5be59b8a9115c
Maryam Omran <maryam.omran03@gmail.com>
2025-05-14 04:31:40 +0300

    weather-app-base

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
-  const [cityInput, setCityInput] = useState('');
+  const [cityInput, setCityInput] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const OPEN_WEATHER_API_KEY = cbbe04b35fb21222d45141cd606f35a2;
 
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .get('https://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${OPEN_WEATHER_API_KEY}')
-      .then ((response) => response.data[0])
-      .then ((cityGeoData) => {
-        return axios.get (
-          'https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric'
+      .get(
+        "https://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${OPEN_WEATHER_API_KEY}"
+      )
+      .then((response) => response.data[0])
+      .then((cityGeoData) => {
+        return axios.get(
+          "https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric"
         );
       })
       .then((response) => {
@@ -27,9 +29,9 @@ const App = () => {
   };
   return (
     <div className="weather-app">
-      <h1>Weather App</h1>
+      <h1>Weather App v1.0.0</h1>
       <form onSubmit={handleSubmit}>
-        <input 
+        <input
           type="text"
           value={cityInput}
           onChange={(event) => setCityInput(event.target.value)}
@@ -40,10 +42,14 @@ const App = () => {
       </form>
       {weatherData && (
         <div className="weather-info">
-          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <h2>
+            {weatherData.name}, {weatherData.sys.country}
+          </h2>
           <p>Temperatrue: {weatherData.main.temp} C</p>
           <img
-            src={'https://openweathermap.org/img/wn/${weatherData.weather[0].icon@2x.png'}
+            src={
+              "https://openweathermap.org/img/wn/${weatherData.weather[0].icon@2x.png"
+            }
             alt={weatherData.weather[0].description}
           />
         </div>
